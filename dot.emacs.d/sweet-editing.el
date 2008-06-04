5b5a6386a56c1136f6891be0c28cbfc57e5c7a7a
Pat Maddox <pat.maddox@gmail.com>
2008-06-04 01:00:04 -0700

    commands to insert new lines where I like them

--- dot.emacs.d/sweet-editing.el
+++ dot.emacs.d/sweet-editing.el
@@ -13,4 +13,18 @@
     (move-to-column column)))
 (define-key global-map (kbd "C-S-d") 'duplicate-line)
 
+(defun insert-and-move-to-new-line ()
+  "Inserts a blank line after the current one"
+  (interactive)
+  (end-of-line)
+  (newline))
+(define-key global-map (kbd "M-<return>") 'insert-and-move-to-new-line)
+
+(defun insert-before-and-move-to-new-line ()
+  "Inserts a blank line before the current one"
+  (interactive)
+  (previous-line)
+  (insert-and-move-to-new-line))
+(define-key global-map (kbd "C-M-<return>") 'insert-before-and-move-to-new-line)
+
 (provide 'sweet-editing)
