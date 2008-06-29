730a37dbcab289f651dbc7dca57350b1b650fcc8
Pat Maddox <pat.maddox@gmail.com>
2008-06-29 13:35:05 -0700

    Find file in project

--- dot.emacs.d/my-ruby-mode.el
+++ dot.emacs.d/my-ruby-mode.el
@@ -4,4 +4,7 @@
 (add-hook 'ruby-mode-hook
           (lambda ()
             (local-set-key (kbd "<return>") 'newline-and-indent)))
+
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+
 (provide 'my-ruby-mode)
--- dot.emacs.d/project-nav.el
+++ dot.emacs.d/project-nav.el
@@ -1,7 +1,11 @@
 (defun generate-tags-for-project ()
   (interactive)
-  (save-excursion
-    (shell-command (concat "cd " (project-root) " && find . -type f | grep -v '\.git' | grep -v '\.svn' | grep -v '^\./vendor' | grep -v '~$' | grep -v '^\.#' | grep -v '^#' | grep -v '#$' | xargs file | grep 'text' | awk '{print $1}' | sed 's/:$//' | xargs etags -l none"))))
+  (let ((prev-dir default-directory))
+    (cd (project-root))
+    (setq files-list (do-filtering (file-filters) (files-with-relative-paths (project-root) (recursive-directory-files (project-root)))))
+    (cd prev-dir)
+    (build-tags-for-files files-list)
+    t))
 
 (defun project-root ()
   (let ((project-file (plv-find-project-file default-directory "")))
@@ -22,6 +26,68 @@
 (defun find-files-in-project ()
   (interactive)
   (choose-file-from-project (lambda (file) (find-file file))))
+
+(defun recursive-directory-files (dir)
+  (let ((dir (file-name-as-directory dir)))
+    (apply 'append
+           (mapcar
+            (lambda (f)
+              (let ((f (concat dir f)))
+                (if (file-directory-p f)
+                    (cons f (recursive-directory-files f))
+                  (list f))))
+            (remove ".."
+                    (remove "."
+                            (condition-case nil
+                                (directory-files dir)
+                              (error nil))))))))
+
+
+(defun build-tags-for-files (files)
+  (shell-command (concat "cd " (project-root) " && etags -l none "
+                         (mapconcat 'identity
+                                    (mapcar (lambda (s) (concat "'" s "'")) files)
+                                    " "))))
+
+;; (defun allowed-file-type-p (f)
+;;   (mapcar
+;;    (lambda (r) )(list "\.rb$"
+;;         "^Rakefile$"
+;;         "\.el$"
+;;         ))
+(defun file-filters ()
+  (list
+   (lambda (s) (file-directory-p s))
+   (lambda (s) (string-match "^\.git" s))
+   (lambda (s) (string-match "^\.svn" s))
+   (lambda (s) (string-match "^vendor\/" s))
+   (lambda (s) (string-match "~$" s))
+   (lambda (s) (string-match "^\.#" s))
+   (lambda (s) (string-match "^#" s))
+   (lambda (s) (string-match "#$" s))
+
+   (lambda (s)
+     (let* ((result
+             (shell-command-to-string (concat "file '" s "' | awk '{$1=\"\"; print}'")))
+            (file-type (replace-regexp-in-string "^[ ]*\\(.*\\)[ \n]*$" "\\1" result)))
+       (not (or (string= "" file-type) (string-match "text" file-type)))))
+))
+
+(defun apply-filter (f list)
+  (remove-if (lambda (s) (funcall f s)) list))
+
+(defun files-with-relative-paths (base list)
+  (mapcar
+   (lambda (s)
+     (replace-regexp-in-string (regexp-quote base) "" s))
+   list))
+
+(defun do-filtering (filters-list list)
+  (let ((filtered-list (apply-filter (car filters-list) list)))
+        (if (cdr filters-list)
+            (do-filtering (cdr filters-list) filtered-list)
+          filtered-list)))
+
   
 (global-set-key (kbd "M-t") 'find-files-in-project)
 
